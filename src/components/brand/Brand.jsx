968953873b17a9ed873a1cc682daaf411422958c
Favourmbata <134517996+Favourmbata@users.noreply.github.com>
2024-09-14 13:36:06 +0100

    completed gpt-3

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,26 @@
+import { google,slack,altlassian,dropbox,shopify } from './import'
 import React from 'react'
+
 import './brand.css'
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className='gpt3__brand section__padding'>
+    <div className=''>
+     <img src={google} alt='google'/>
+    </div>
+    <div className=''>
+     <img src={slack} alt='slack'/>
+    </div>
+    <div className=''>
+     <img src={altlassian} alt='altlasian'/>
+    </div>
+    <div className=''>
+     <img src={dropbox} alt='dropbox'/>
+    </div>
+    <div className=''>
+     <img src={shopify} alt='shopify'/>
+    </div>
+    </div>
   )
 }
 
---
+++ src/components/brand/import.js
@@ -0,0 +1,13 @@
+import google from "../../assets/google (1).png"
+import slack from "../../assets/slack.png"
+import altlassian from "../../assets/atlassian.png"
+import dropbox from "../../assets/dropbox.png"
+import shopify from "../../assets/shopify.png"
+
+export {
+    google,
+    slack,
+    altlassian,
+    dropbox,
+    shopify,
+}
